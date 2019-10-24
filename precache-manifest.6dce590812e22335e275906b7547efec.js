f5ffd62766a2057ec18728ab376db6059898e889
PikaThink <pika@pika.wtf>
2019-10-24 04:57:12 +0000

    Updates

---
+++ precache-manifest.6dce590812e22335e275906b7547efec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3976c08381b3881e667ec49684c0982",
+    "url": "/speccanal/index.html"
+  },
+  {
+    "revision": "647a37b48eefbd3de162",
+    "url": "/speccanal/static/css/main.8465155b.chunk.css"
+  },
+  {
+    "revision": "33a94d5fc6fdf217a278",
+    "url": "/speccanal/static/js/2.db96a204.chunk.js"
+  },
+  {
+    "revision": "647a37b48eefbd3de162",
+    "url": "/speccanal/static/js/main.a99d5f83.chunk.js"
+  },
+  {
+    "revision": "96b7e92aeef7e2860fe0",
+    "url": "/speccanal/static/js/runtime-main.35349d1d.js"
+  },
+  {
+    "revision": "544b30d9aec0318e555a74e63510fce4",
+    "url": "/speccanal/static/media/pikacry.544b30d9.gif"
+  },
+  {
+    "revision": "a1450d165605eb7f0dd21a6241ad863f",
+    "url": "/speccanal/static/media/pikathink.a1450d16.png"
+  }
+]);
